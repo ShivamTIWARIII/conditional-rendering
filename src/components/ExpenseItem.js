5afc087ea516473e114463e65b297cc1d3cf711a
ShivamTIWARIII <shivamtiwariii1212@gmail.com>
2023-11-18 13:12:49 +0530

    React1st

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <h1>Expense Item!</h1>
+            <h4>Food Rs 10</h4>
+            <h4>Petrol Rs 100</h4>
+            <h4>Movies Rs 200</h4>
+        </div>
+        
+    )
+}
+export default ExpenseItem;
