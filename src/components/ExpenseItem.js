f35d3e5f9443a7dfb873ba1dd111fdc048a16589
ShivamTIWARIII <shivamtiwariii1212@gmail.com>
2023-11-20 12:37:04 +0530

    react-eventHandler

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -10,12 +10,20 @@ const ExpenseItem=(props)=>{
   //const month=props.date.toLocaleString('en-US',{month:'long'});
   //const day=props.date.toLocaleString('en-US',{day:'2-digit'});
   //const year=props.date.getFullYear();
+  const clickHandler=()=>{
+    console.log("Clicked!!!!")
+  }
+  const deleteHandler=()=>{
+    console.log("Deleted!!!")
+  }
     return (
         <div className='expense-item'>
           <ExpenseDate date={props.date}></ExpenseDate>
            <ExpenseDetails amount={props.amount} location={props.location} title={props.title} ></ExpenseDetails>
-          
+           <button onClick={clickHandler}>Change Title</button>
+           <button onClick={deleteHandler}>Delete</button>
         </div>
+        
         
     )
 }
