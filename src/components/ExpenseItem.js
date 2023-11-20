97bd166a2a00362f1ca47e1cfe40961a528cea4b
ShivamTIWARIII <shivamtiwariii1212@gmail.com>
2023-11-20 15:35:27 +0530

    react-update

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react'
 import ExpenseDetails from './ExpenseDetails';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import './ExpenseItem.css'
@@ -10,18 +11,23 @@ const ExpenseItem=(props)=>{
   //const month=props.date.toLocaleString('en-US',{month:'long'});
   //const day=props.date.toLocaleString('en-US',{day:'2-digit'});
   //const year=props.date.getFullYear();
+  const [title,setTitle]=useState(props.title)
+  const [amount,setAmount]=useState(props.amount)
+ 
   const clickHandler=()=>{
-    console.log("Clicked!!!!")
+    setTitle('Updated!!')
+    console.log(title)
   }
   const deleteHandler=()=>{
-    console.log("Deleted!!!")
+    setAmount(100)
+    console.log(amount)
   }
     return (
         <div className='expense-item'>
           <ExpenseDate date={props.date}></ExpenseDate>
-           <ExpenseDetails amount={props.amount} location={props.location} title={props.title} ></ExpenseDetails>
+           <ExpenseDetails amount={amount} location={props.location} title={title} ></ExpenseDetails>
            <button onClick={clickHandler}>Change Title</button>
-           <button onClick={deleteHandler}>Delete</button>
+           <button onClick={deleteHandler}>Update Amount</button>
         </div>
         
         
