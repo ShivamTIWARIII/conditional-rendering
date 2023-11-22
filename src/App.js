36e0d143351a757c772097bb66b68b4aee8531f3
ShivamTIWARIII <shivamtiwariii1212@gmail.com>
2023-11-22 12:05:37 +0530

    react-dynamic

--- src/App.js
+++ src/App.js
@@ -1,63 +1,53 @@
+import React, { useState } from 'react';
 import ExpenseItem from './components/ExpenseItem';
-import ExpenseForm from './components/ExpenseForm'
-const App=()=> {
-  const expense = [
+import NewExpense from './components/NewExpense';
+
+const App = () => {
+  const [expenses, setExpenses] = useState([
     {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      LocationOfExpenditure: 'Jaipur'
+      LocationOfExpenditure: 'Jaipur',
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) ,LocationOfExpenditure: 'Mumbai'},
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12), LocationOfExpenditure: 'Mumbai' },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      LocationOfExpenditure: 'Lucknow'
+      LocationOfExpenditure: 'Lucknow',
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
-      LocationOfExpenditure: 'Delhi'
+      LocationOfExpenditure: 'Delhi',
     },
-  ];
+  ]);
+
+  const addExpenseHandler = (expense) => {
+    // Updating the expenses state with the new expense
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
+  };
+
   return (
     <div>
       <h1>It's my first react js project!</h1>
-      <ExpenseForm></ExpenseForm>
-      <ExpenseItem 
-      title={expense[0].title}
-      amount={expense[0].amount}
-      date={expense[0].date}
-      location={expense[0].LocationOfExpenditure}
-      ></ExpenseItem>
-      <ExpenseItem
-      title={expense[1].title}
-      amount={expense[1].amount}
-      date={expense[1].date}
-      location={expense[1].LocationOfExpenditure}
-      ></ExpenseItem>
-      <ExpenseItem
-      title={expense[2].title}
-      amount={expense[2].amount}
-      date={expense[2].date}
-      location={expense[2].LocationOfExpenditure}
-      ></ExpenseItem>
-
-<ExpenseItem
-      title={expense[3].title}
-      amount={expense[3].amount}
-      date={expense[3].date}
-      location={expense[3].LocationOfExpenditure}
-      ></ExpenseItem>
-      
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          location={expense.LocationOfExpenditure}
+        ></ExpenseItem>
+      ))}
     </div>
-   
   );
-}
+};
 
 export default App;
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import './ExpenseForm.css'
-function ExpenseForm(){
+function ExpenseForm(props){
     const [enteredTitle,setTitle]=useState('')
     const [enteredAmount,setAmount]=useState('')
     const [enteredDate,setDate]=useState('')
@@ -44,26 +44,33 @@ function ExpenseForm(){
             amount:enteredAmount,
             date:new Date(enteredDate)
         }
-        console.log(expenseData)
+       // console.log(expenseData)
+       props.onSaveExpenseData(expenseData)
+        
+    setTitle('')
+    setAmount('')
+    setDate('')
 
     }
+
+
     return(
      
         <form onSubmit={submitHandler}>
             <div className='new-expense__controls '>
                 <div className='new-expense__control'>
             <label>Expense title
-                <input type='text' name='title' onChange={titleChangeHandler}></input>
+                <input type='text' name='title' value={enteredTitle} onChange={titleChangeHandler}></input>
             </label>
             </div>
             <div className='new-expense__control'>
             <label>Expense Amount
-                <input type='number' name='amount' min='0.01' step='0.01' onChange={amountChangeHandler}></input>
+                <input type='number' value={enteredAmount} name='amount' min='0.01' step='0.01' onChange={amountChangeHandler}></input>
             </label>
             </div>
             <div className='new-expense__control '>
             <label>Date
-                <input type='date' name='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler}></input>
+                <input type='date' value={enteredDate} name='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler}></input>
             </label>
             </div>
             </div>
